9c3d7bdf1774dfd65ec7177cf3861c0e467db6e7
KarenCalle98 <karen.calle.397@gmail.com>
2024-08-12 22:33:05 -0500

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  //{ path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
